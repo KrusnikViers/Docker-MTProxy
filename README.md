9ed81944e53fd6a7d6ec90f6b5e0bfb8a4f76a53
Ivan Chernousov <civ-viers@yandex-team.ru>
2018-08-01 05:14:50 +0700

    Huge refactoring

--- README.md
+++ README.md
@@ -1,38 +1,44 @@
-# MTProto 1.0
-Easy-configurable docker image for the MTProto proxy server.
+# MTProxy v1.0
+Lightweight and easy to set up docker image for MTProto proxy server.
 
-[![](https://images.microbadger.com/badges/image/viers/mtproto.svg)](https://microbadger.com/images/viers/mtproto "Image size")
+[![Docker Build Status](https://img.shields.io/docker/build/viers/mtproxy.svg?style=flat-square)](https://hub.docker.com/r/viers/mtproxy/)
+[![](https://images.microbadger.com/badges/image/viers/mtproxy.svg)](https://microbadger.com/images/viers/mtproxy "Image size")
+
+
+Binary inside: [f9158e3129efd4c from 19 Jul 2018](https://github.com/TelegramMessenger/MTProxy/commit/f9158e3129efd4ccdc291aefb840209791226a77)
 
 ### How to run:
-All you need is an installed docker and filled json configuration file.
+All you need is an installed docker. Server can be configured with the special json dictionary file, where all the fields are top level key-value pairs. If the field is absent, it's value is set to default.
 
-Configuration file format:
+* `keys`: An array of client keys, if you have generated some before. By default, this array is empty. 
+* `new_keys`: Number of new client keys to generate. If you have no existing keys, you should generate at least one. Default value: 1
+* `update_hours`: How often telegram servers list will be updated. Default value: 12.
+* `ip`: Server external IP. Necessary for servers behind NAT, also used in invite links, if server URL was not provided. By default is empty.
+* `url`: Server URL, if any, to be used in invite links. Default value: same as `ip`.
+* `port`: Exposed server port for invite links. Default value: 443.
+* `tag`: Your proxy tag, optionally received from [@MTProxyBot](https://t.me/MTProxybot). By default is empty.
+
+Configuration file example:
 ```json
 {
-    "existing_keys": [
+    "keys": [
         "deadbeefdeadbeefdeadbeefdeadbeef"
     ],
-    "keys_to_generate": 1,
-    "update_period_hours": 24,
-    "server_ip": "127.0.0.1",
-    "server_port": 443,
+    "new_keys": 1,
+    "update_hours": 12,
+    "ip": "127.0.0.1",
+    "url": "my-tgproxy.com",
+    "port": 443,
     "tag": "1234554321"
 }
-
 ```
-* existing_keys: An array of client keys, if you have generated some before. 
-* keys_to_generate: Number of new client keys to generate. Default: 1
-* update_period_hours: How often server will be updating core configuration. Default: 24.
-* server_url: Server URL (necessary to generate invite link).
-* server_port: Server port. Default: 443.
-* tag: Your proxy tag.
 
-Command to launch server itself:
+To be applied, you should mount this file into docker container as `/configuration.json`. Recommended command to launch container:
 
-`docker run -d --restart always --net "host" -v [configuration path]:/configuration.json --name mtproto viers/mtproto`
+`docker run -d --restart always -p [port]:443 -v [configuration path]:/configuration.json --name mtproxy viers/mtproxy`
 
-, where [configuration path] is a full path to your configuration file.
+, where `[port]` is the server port, and `[configuration path]` is a full path to your configuration file.
 
-To see the logs, generated and existing keys and invite links: `docker logs --follow mtproto`.
+Generated keys and all invite links will be printed in container log. To see the logs, use: `docker logs --follow mtproxy`.
 
 To collect the server stats: `docker exec mtproto curl http://localhost:80/stats`
--- src/entry.py
+++ src/entry.py
@@ -7,60 +7,80 @@ import os
 import socket
 import subprocess
 
-from update import download_from_core
+import update
 
 
-# Read configuration
-with open('/configuration.json') as configuration_file:
-    configuration = json.load(configuration_file)
+# Read configuration, if any.
+configuration = {}
+configuration_path = '/configuration.json'
+if os.path.isfile(configuration_path):
+    with open('/configuration.json') as configuration_file:
+        configuration = json.load(configuration_file)
+else:
+    print('Warning! No configuration file has been provided. Server will run with default options.')
 
-    keys = configuration.get('existing_keys', [])
-    keys_to_generate = configuration.get('keys_to_generate', 1)
-    update_period_hours = configuration.get('update_period_hours', 24)
-    server_ip = configuration['server_ip']
-    server_port = configuration.get('server_port', 443)
-    tag = configuration.get('proxy_tag', '')
-    tag = (' -P ' + tag) if tag else ''
+# Define proxy server parameters
+keys = configuration.get('keys', [])
+new_keys = configuration.get('new_keys', 1)
+update_hours = configuration.get('update_hours', 12)
+ip = configuration.get('ip', None)
+url = configuration.get('url', ip)
+port = configuration.get('port', 443)
+tag = configuration.get('tag', None)
 
 # Generate and print client keys.
-for i in range(0, keys_to_generate):
+for i in range(0, new_keys):
     key_string = binascii.b2a_hex(os.urandom(16)).decode('UTF-8')
     keys.append(key_string)
 
-keys_string = ''
-template_url = 'tg://proxy?server={}&port={}&secret={{}}'.format(server_ip, server_port)
-print('---------------------------')
+invite_url = 'tg://proxy?server={}&port={}&secret={}'
+print('----- Client Keys Data -----')
+if not url:
+    print('Warning! No server url or ip has been provided. Invite links will not be generated.')
+print('----------------------------')
 for key in keys:
-    print('Key to be used: {}'.format(key))
-    print('Invite link: ' + template_url.format(key))
-    print('---------------------------')
-    keys_string += ' -S ' + key
+    print('Key: {}'.format(key))
+    if url:
+        print('Invite link: ' + invite_url.format(url, port, key))
+    print('----------------------------')
 
-# Find out local IP with internet access.
-test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-test_socket.connect(("8.8.8.8", 80))
-local_ip = test_socket.getsockname()[0]
-test_socket.close()
+# Download necessary data from telegram core.
+update.download(update.secret_name)
+update.download(update.servers_conf_name)
 
-# Download configuration data from telegram core.
-download_from_core('secret')
-download_from_core('proxy.conf')
-
-# Create cron job, if necessary
+# Create cron job, if necessary.
 cron = crontab.CronTab(user=True)
-existing_jobs = list(cron.find_comment('mtp'))
+existing_jobs = list(cron.find_comment('mtproxy'))
 if not existing_jobs:
-    job = cron.new(command='python /src/update.py &> /last_log.txt', comment='mtp')
-    job.hour.every(update_period_hours)
+    job = cron.new(command='python /src/update.py &> /server/last_update_log.txt', comment='mtproxy')
+    job.hour.every(update_hours)
     cron.write()
+else:
+    print('Cron job has already been planned.')
+print('Current cron setup:')
+for job in cron:
+    print(job)
 
-# Launch server.
-stat_port = 80
-secret_file = '/server/secret'
-proxy_configuration_file = '/server/proxy.conf'
-workers_count = 1
+# Generate command for mtproxy binary, set system user, stat and proxy ports:
+command = update.server_dir + 'mtproto-proxy -u nobody -p 80 -H 443'
+# Client keys:
+command += ' ' + ' '.join(['-S {}'.format(key) for key in keys])
+# Proxy server tag:
+if tag:
+    command += ' -T {}'.format(tag)
+# NAT information:
+if ip:
+    # Find local IP with internet access.
+    test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    test_socket.connect(("8.8.8.8", 80))
+    local_ip = test_socket.getsockname()[0]
+    test_socket.close()
+    print('Local IP defined as {}'.format(local_ip))
+    if local_ip != ip:
+        command += ' --nat-info {}:{}'.format(local_ip, ip)
+# Telegram configuration files and workers count:
+command += ' --aes-pwd {} {} -M 1'.format(update.server_dir + update.secret_name,
+                                          update.server_dir + update.servers_conf_name)
+print('Launching:\n{}\n\n'.format(command))
 
-command = '/server/mtproto-proxy -u nobody -p {} -H {} {} {} --nat-info {}:{} --aes-pwd {} {} -M {}'.format(
-    stat_port, server_port, keys_string, tag, local_ip, server_ip, secret_file, proxy_configuration_file, workers_count)
-print('launching:\n' + command)
 subprocess.run(command, shell=True)
