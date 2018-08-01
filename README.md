b079670ca0eccc81e270692f196ea3bca2490fa5
Ivan Chernousov <civ-viers@yandex-team.ru>
2018-08-02 01:42:21 +0700

    Add interactive configuration file

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # MTProxy v1.0.1
 Lightweight and easy to set up docker image for MTProto proxy server.
 
-[![](https://images.microbadger.com/badges/image/viers/mtproxy.svg)](https://microbadger.com/images/viers/mtproxy "Image size")
-
+[![Docker Build Status](https://img.shields.io/docker/build/viers/mtproxy.svg)](https://hub.docker.com/r/viers/mtproxy/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/viers/mtproxy.svg)](https://hub.docker.com/r/viers/mtproxy/)
+[![MicroBadger Size](https://images.microbadger.com/badges/image/viers/mtproxy.svg)](https://microbadger.com/images/viers/mtproxy)
 
 Binary inside: [f9158e3129efd4c from 19 Jul 2018](https://github.com/TelegramMessenger/MTProxy/commit/f9158e3129efd4ccdc291aefb840209791226a77)
 
@@ -17,27 +18,18 @@ All you need is an installed docker. Server can be configured with the special j
 * `port`: Exposed server port for invite links. Default value: 443.
 * `tag`: Your proxy tag, optionally received from [@MTProxyBot](https://t.me/MTProxybot). By default is empty.
 
-Configuration file example:
-```json
-{
-    "keys": [
-        "deadbeefdeadbeefdeadbeefdeadbeef"
-    ],
-    "new_keys": 1,
-    "update_hours": 12,
-    "ip": "127.0.0.1",
-    "url": "my-tgproxy.com",
-    "port": 443,
-    "tag": "1234554321"
-}
-```
+To be applied, you should mount this file into docker container as `/configuration.json`. You could also mount file with empty json dictionary, to let server generate file with default values for you at the first launch.
 
-To be applied, you should mount this file into docker container as `/configuration.json`. Recommended command to launch container:
+**Important!** After file is mounted, it could be modified from inside container. In particular, `new_keys` will be set to 0, and generated keys will be placed into `keys` section. This will allow you to restart container safely without losing any generated data. Also, you may update data in configuration file yourself - in that case, new values will be applied at the next update or after manual restart.
+
+---
+
+Recommended command to launch container:
 
 `docker run -d --restart always -p [port]:443 -v [configuration path]:/configuration.json --name mtproxy viers/mtproxy`
 
-, where `[port]` is the server port, and `[configuration path]` is a full path to your configuration file.
+where `[port]` is the server port, and `[configuration path]` is a full path to your configuration file. Generated keys and all invite links will be printed in container log.
 
-Generated keys and all invite links will be printed in container log. To see the logs, use: `docker logs --follow mtproxy`.
+To see the logs, use: `docker logs --follow mtproxy`.
 
-To collect the server stats: `docker exec mtproto curl http://localhost:80/stats`
+To collect server stats, use: `docker exec mtproxy curl http://localhost:80/stats`
--- src/entry.py
+++ src/entry.py
@@ -22,66 +22,78 @@ def download(path: str, source_url: str):
         print('Download to {} failed: {}'.format(path, download_request.text))
 
 
-# Read configuration, if any.
-configuration = {}
-configuration_path = '/configuration.json'
-if os.path.isfile(configuration_path):
-    with open('/configuration.json') as configuration_file:
-        configuration = json.load(configuration_file)
-else:
-    print('Warning! No configuration file has been provided. Server will run with default options')
-
-# Define proxy server parameters.
-keys = configuration.get('keys', [])
-new_keys = configuration.get('new_keys', 1)
-update_hours = configuration.get('update_hours', 12)
-ip = configuration.get('ip', None)
-url = configuration.get('url', ip)
-port = configuration.get('port', 443)
-tag = configuration.get('tag', None)
-
-# Base command for mtproxy binary, with system user, stat and proxy ports.
-command = '/server/mtproto-proxy -u nobody -p 80 -H 443'
-if tag:
-    command += ' -T {}'.format(tag)
-
-# Generate and print client keys.
-for i in range(0, new_keys):
-    keys.append(binascii.b2a_hex(os.urandom(16)).decode('UTF-8'))
-
-# Print client keys with invite keys.
-if not url:
-    print('Warning! No server url or ip has been provided. Invite links will not be generated.')
-for key in keys:
-    print('----------------------------')
-    print('Key: {}'.format(key))
-    if url:
-        print('Link: tg://proxy?server={}&port={}&secret={}'.format(url, port, key))
-    print('----------------------------')
-    command += ' -S ' + key
-
-# Check if server is behind NAT (external ip should be provided).
-if ip:
-    test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    test_socket.connect(("8.8.8.8", 80))  # Use Google DNS as remote node.
-    local_ip = test_socket.getsockname()[0]
-    test_socket.close()
-    if local_ip != ip:
-        print('Server local and external IP mismatch: {} vs {}'.format(local_ip, ip))
-        command += ' --nat-info {}:{}'.format(local_ip, ip)
-
-# Configuration files.
+# Download core token, to access telegram servers.
 secret_path = '/server/secret'
-proxy_list_path = '/server/proxy.conf'
 download(secret_path, 'https://core.telegram.org/getProxySecret/proxy-secret')
-command += ' --aes-pwd {} {} -M 1'.format(secret_path, proxy_list_path)
 
-seconds_to_wait = update_hours * 3600
-server_process = None
-
-# Outer loop: download a proxy servers list, and run the server for |update_hours|.
+# Outer loop: update local and remote configuration and run inner server loop.
 while True:
+    # Read configuration, if any.
+    configuration = {}
+    configuration_path = '/configuration.json'
+    if os.path.isfile(configuration_path):
+        with open(configuration_path) as configuration_file:
+            configuration = json.load(configuration_file)
+    else:
+        print('Warning! No configuration file has been provided. Server will run with default options')
+
+    # Define proxy server parameters.
+    keys = configuration.get('keys', [])
+    new_keys = configuration.get('new_keys', 1)
+    update_hours = configuration.get('update_hours', 12)
+    ip = configuration.get('ip', '')
+    url = configuration.get('url', ip)
+    port = configuration.get('port', 443)
+    tag = configuration.get('tag', '')
+
+    # Base command for mtproxy binary, with system user, stat and proxy ports.
+    command = '/server/mtproto-proxy -u nobody -p 80 -H 443'
+    if tag:
+        command += ' -T {}'.format(tag)
+
+    # Generate and print client keys.
+    for i in range(0, new_keys):
+        keys.append(binascii.b2a_hex(os.urandom(16)).decode('UTF-8'))
+
+    # Print client keys with invite keys.
+    if not url:
+        print('Warning! No server url or ip has been provided. Invite links will not be generated.')
+    for key in keys:
+        print('>>>>>>>>>>')
+        print('Key: {}'.format(key))
+        if url:
+            print('Link: tg://proxy?server={}&port={}&secret={}'.format(url, port, key))
+        print('<<<<<<<<<<')
+        command += ' -S ' + key
+
+    # Check if server is behind NAT (external ip should be provided).
+    if ip:
+        test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        test_socket.connect(("8.8.8.8", 80))  # Use Google DNS as remote node.
+        local_ip = test_socket.getsockname()[0]
+        test_socket.close()
+        if local_ip != ip:
+            print('Server local and external IP mismatch: {} vs {}'.format(local_ip, ip))
+            command += ' --nat-info {}:{}'.format(local_ip, ip)
+
+    # Configuration files.
+    proxy_list_path = '/server/proxy.conf'
     download(proxy_list_path, 'https://core.telegram.org/getProxyConfig/proxy-multi.conf')
+    command += ' --aes-pwd {} {} -M 1'.format(secret_path, proxy_list_path)
+
+    # Write prepared values into local configuration.
+    configuration['keys'] = keys
+    configuration['new_keys'] = 0
+    configuration['update_hours'] = update_hours
+    configuration['ip'] = ip
+    configuration['url'] = url
+    configuration['port'] = port
+    configuration['tag'] = tag
+    with open(configuration_path, 'w') as configuration_file:
+        json.dump(configuration, configuration_file, indent='  ')
+    print('Configuration file updated: {}b'.format(os.path.getsize(configuration_path)))
+
+    seconds_to_wait = update_hours * 3600
     start_time = time.time()
 
     # Inner loop: running and restarting the server, if it has crashed until the update time.
